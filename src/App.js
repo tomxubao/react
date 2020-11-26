d4c4ec33c76342f27a3ac8853394a13bdb7e3b6d
tomxubao <734121713@qq.com>
2020-11-27 00:09:35 +0800

    react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/**
+ * 应用跟组件
+ */
+//将组件暴露出去
+import React ,{Component} from 'react'
+
+
+export default class  App extends Component{
+    render (){
+        return(
+            <div id="root">App</div>
+        )
+    }
+}
